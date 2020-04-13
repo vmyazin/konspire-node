75749ff36b471a26cf478c8c2425d2dd8a1fe161
Vasily Myazin <vm@konspiredesign.com>
2020-04-12 20:18:39 -0400

    Set up body class

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Konspire Design' });
+  res.render('index', {
+    title: 'Konspire Design',
+    body_class: "home"
+  });
 });
 
 router.get('/about', (req, res) => {
-  res.render('about', { title: 'About Konspire Design' });
+  res.render('about', {
+    title: 'About Konspire Design',
+    body_class: "about"
+  });
 });
 
 router.get('/portfolio', (req, res) => {
-  res.render('portfolio', { title: 'Portfolio Konspire Design' });
+  res.render('portfolio', {
+    title: 'Portfolio Konspire Design',
+    body_class: "portfolio"
+  });
 });
 
 module.exports = router;
