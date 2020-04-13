bfe670b2fc0ae0becbcd92f6b97b634692fdda7d
Vasily Myazin <vm@konspiredesign.com>
2020-04-12 21:01:44 -0400

    Fix async Flickr API with Misha

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+async function getFlickrData() {
+  try {
+    const response = await got('https://api.flickr.com/services/feeds/photoset.gne?set=72157623352223751&nsid=33129098@N06&lang=en-us&format=json&nojsoncallback=1');
+    return JSON.parse(response.body);
+    //=> '<!doctype html> ...'
+  } catch (error) {
+      console.log(error);
+      //=> 'Internal server error ...'
+  }
+}
+
+router.get('/', async function(req, res, next) {
+  const data = await getFlickrData();
+  console.log(data);
+
   res.render('index', {
     title: 'Konspire Design',
-    body_class: "home"
+    body_class: "home",
+    data: data
   });
 });
+
+const got = require('got');
+ 
+function getFlickrData2() {
+  request.get("https://api.flickr.com/services/feeds/photoset.gne?set=72157623352223751&nsid=33129098@N06&lang=en-us&format=json&nojsoncallback=1", (err, response, body) => {
+    if (err) {
+      return next(err);
+    }
+    // enduro.api.temper.render('about', JSON.parse(body))
+    //   .then((output) => {
+    //       res.send(output)
+    // })
+    // res.send({data: JSON.parse(body)});
+    return JSON.parse(body); 
+  });
+}
 
 router.get('/about', (req, res) => {
   res.render('about', {
