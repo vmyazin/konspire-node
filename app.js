413a632b179c0b863bd1fcc2b05bae6a358b56dd
Vasily Myazin <vm@konspiredesign.com>
2021-11-24 18:19:59 -0300

    App.js Fix

--- app.js
+++ app.js
@@ -23,31 +23,36 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: __dirname + '/scss', //where the sass files are 
-  dest: __dirname + '/public/stylesheets', //where css should go
-  debug: true,
-  indentedSyntax: false,
-  outputStyle: 'compressed',
-  prefix: '/stylesheets'
-}));
+app.use(
+  sassMiddleware({
+    src: __dirname + '/scss', //where the sass files are
+    dest: __dirname + '/public/stylesheets', //where css should go
+    debug: true,
+    indentedSyntax: false,
+    outputStyle: 'compressed',
+    prefix: '/stylesheets',
+  })
+);
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/', function (req, res, next) {
-  req.config = app.locals;
-  next();
-}, indexRouter);
+app.use(
+  '/',
+  function (req, res, next) {
+    req.config = app.locals;
+    next();
+  },
+  indexRouter
+);
 
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -59,3 +64,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:3000`);
+});
